56b79f4c5004dc846df00676c6b5d6802a2d23d9
Ryato-03 <116544189+Ryato-03@users.noreply.github.com>
2022-11-10 17:19:45 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -7,10 +7,6 @@ int main(){
     cin >> a >> b >> c;
 
 //Qui va scritto il codice per eseguire i calcoli richiesti
-    float AreaTriangolo = (a * b) / 2;
-    float AreaQuadrato = a * a;
-    float AreaRettangolo = a * b;
-    float  AreaTrapezio = ((a + a) * c) / 2;
     cout << (a * b) / 2 << endl;
     cout << a * a << endl;
     cout << a * b << endl;
