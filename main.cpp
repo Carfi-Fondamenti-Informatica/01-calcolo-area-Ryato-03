7f57ed00d9116b459e5d4b5a491d62b451f63780
Ryato-03 <116544189+Ryato-03@users.noreply.github.com>
2022-11-10 16:56:07 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -4,15 +4,15 @@ using namespace std;
 int main(){
 //Qui vanno definite le variabili
     float a, b, c;
-    cout << "Inserire i dati per completare l'operazione:" << endl;
+//    cout << "Inserire i dati per completare l'operazione:" << endl;
     cin >> a >> b >> c;
 
 //Qui va scritto il codice per eseguire i calcoli richiesti
     float AreaTriangolo = (a * b) / 2;
     float AreaQuadrato = a * a;
     float AreaRettangolo = a * b;
-    float  AreaTrapezio = ((a + b) * c) / 2;
-    cout << "Area Triangolo = " << AreaTriangolo << endl << "Area Quadrato = " << AreaQuadrato << endl;
-    cout << "Area Rettangolo = " << AreaRettangolo << endl  << "Area Trapezio = " << AreaTrapezio << endl;
+    float  AreaTrapezio = ((a + a) * c) / 2;
+    cout << AreaTriangolo << endl << AreaQuadrato << endl;
+    cout << AreaRettangolo << endl << AreaTrapezio << endl;
     return 0;
 }
